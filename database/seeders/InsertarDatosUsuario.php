235a4e5245a474d959dca05ca9a512b0b9093eb9
Esteban Lagos <elagosquintanilla@gmail.com>
2022-08-13 00:19:40 -0400

    archivo seeder usuario

--- database/seeders/InsertarDatosUsuario.php
+++ database/seeders/InsertarDatosUsuario.php
@@ -14,6 +14,25 @@ class InsertarDatosUsuario extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('Usuario')->insert(array(
+            [  
+              'nombre_completo' => 'Karen Perez',
+              'email' => 'kperez@email.com',
+              'password' => '123456',
+              'estado' => 'true'
+             ],
+             [  
+                'nombre_completo' => 'Daniel Soto',
+                'email' => 'dsoto@email.com',
+                'password' => '123456',
+                'estado' => 'true'
+              ],
+              [  
+                'nombre_completo' => 'Javiera Gonzalez',
+                'email' => 'jgonzalez@email.com',
+                'password' => '123456',
+                'estado' => 'true'
+              ]
+          ));
     }
 }
