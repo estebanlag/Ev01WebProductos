03c6864c90161ef8681b15fd5c225bfdb3138bee
seba5197 <sebastian5197@gmail.com>
2022-08-13 00:23:01 -0400

    prod suc

--- database/seeders/InsertarDatosProductoSucursal.php
+++ database/seeders/InsertarDatosProductoSucursal.php
@@ -14,6 +14,37 @@ class InsertarDatosProductoSucursal extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('ProductoSucursal')->insert(array(
+
+            [
+                'cod_Producto_Sucursal'=>'1/1',
+                'id_producto'=>'1',
+                'id_categoria'=>'1',
+                'Cantidad'=>'12',
+                'Precio'=>'1500',
+                'estado'=>'activo'
+                
+            ],
+            [
+                'cod_Producto_Sucursal'=>'2/2',
+                'id_producto'=>'2',
+                'id_categoria'=>'2',
+                'Cantidad'=>'1',
+                'Precio'=>'2500',
+                'estado'=>'activo'
+                
+            ],
+            [
+                'cod_Producto_Sucursal'=>'3/3',
+                'id_producto'=>'3',
+                'id_categoria'=>'3',
+                'Cantidad'=>'2',
+                'Precio'=>'1200',
+                'estado'=>'no activo'
+                
+            ],
+
+        
+            ));
     }
 }
