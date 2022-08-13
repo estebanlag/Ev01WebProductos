bccfe8e9c42fcfe105b59d51670d17e9b7a765a2
seba5197 <sebastian5197@gmail.com>
2022-08-13 00:18:04 -0400

    prod cat

--- database/seeders/InsertarDatosProductoCategoria.php
+++ database/seeders/InsertarDatosProductoCategoria.php
@@ -15,5 +15,27 @@ class InsertarDatosProductoCategoria extends Seeder
     public function run()
     {
         //
+        DB::table('ProductoCategoria')->insert(array(
+
+            [
+                'id_producto'=>'1',
+                'id_categoria'=>'1'
+                
+            ],
+
+            [
+                'id_producto'=>'2',
+                'id_categoria'=>'2'
+                
+            ],
+
+            [
+                'id_producto'=>'3',
+                'id_categoria'=>'3'
+                
+            ]
+        
+        
+            ));
     }
 }
