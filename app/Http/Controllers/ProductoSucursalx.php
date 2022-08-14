96ba37cd1ceefb157bdb8fda028f79fe4aa3742f
seba5197 <sebastian5197@gmail.com>
2022-08-14 16:26:36 -0400

    prod sucursal

--- app/Http/Controllers/ProductoSucursalx.php
+++ app/Http/Controllers/ProductoSucursalx.php
@@ -14,29 +14,15 @@ class ProductoSucursalx extends Controller
         $prodsucursal = ProductoSucursal::get();
        // dd($prodsucursal);
         
-        print_r($prodsucursal);
+       // print_r($prodsucursal);
 
 
-        $sucursal = DB::table('sucursal')->get();
+        $sucursales = DB::table('sucursal')->get();
         $productos = DB::table('producto')->get();
+       
       
-        // ESTO DEBE VENIR EN LA VISTA
-        //echo "ingresa cantidad <br><input type='number' class='form-control' placeholder='ingresa cantidad'><br>";
-       // echo "ingresa valor <br><input type='number' class='form-control' placeholder='ingresa valor'><br>";
-        //echo "<select name='sucursal'>";
-        //foreach ($sucursal as $valor) {
-        //       echo "<option value=' {$valor->id}'>{$valor->nombre}</option>";
-         //  }
-        //  echo "</select><br>";
-      //
-        //  echo "<select name='productos'>";
-        //foreach ($productos as $valor) {
-         //      echo "<option value=' {$valor->id}'>{$valor->nombre}</option>";
-          // }
-          //echo "</select><br>";
-          
         
-        
+          return view('productosucursal',['sucursales' => $sucursales],['productos' => $productos]);    
       
     }
 
--- resources/views/productosucursal.blade.php
+++ resources/views/productosucursal.blade.php
@@ -6,46 +6,43 @@
 <h2  class="text-center mt-4 mb-4">Asignar Productos a Sucursal</h2>
 @stop
 
+
 @section('content')
 <div class="container">
 <form method="post" action="{{ url('/guardarproductoSucursal') }}">
   <div class="mb-3">
-    <label for="nombre" class="form-label">Codigo Producto</label>
+    <label for="nombre" class="form-label">Codigo Producto sucursal</label>
     <input type="text" class="form-control" id="codigo" name="codigo">
   </div>
   <div class="mb-3">
-    <label for="nombre" class="form-label">Nombre Producto</label>
-    <input type="text" class="form-control" id="nombre" name="nombre">
+  <label for="sucursal" class="form-label">Producto</label>
+  <select class="form-select" aria-label="producto" id="producto" name="producto">
+    <option selected disabled>Seleccione un producto</option>
+@foreach($productos as $producto)
+
+    <option value="{{$producto->id}}">{{$producto->nombre}}</option>
+ @endforeach
+  </select>
   </div>
+ 
   <div class="mb-3">
     <label for="cantidad" class="form-label">Cantidad</label>
     <input type="text" class="form-control" id="cantidad" name="cantidad">
   </div>
+ 
   <div class="mb-3">
-  <label for="categoria" class="form-label">Categoría</label>
-  <select class="form-select" aria-label="categoría" id="categoria" name="categoria">
-    <option selected>Seleccione una Categoría</option>
-    <option value="categoria 1">Categoria1</option>
-    <option value="categoria 2">Categoria2</option>
-    <option value="categoria 3">Categoria3</option>
-  </select>
+    <label for="cantidad" class="form-label">Precio</label>
+    <input type="text" class="form-control" id="precio" name="precio">
   </div>
+  
   <div class="mb-3">
   <label for="sucursal" class="form-label">Sucursal</label>
   <select class="form-select" aria-label="sucursal" id="sucursal" name="sucursal">
-    <option selected>Seleccione una Sucursal</option>
-    <option value="sucursal 1">Sucursal1</option>
-    <option value="sucursal 2">Sucursal2</option>
-    <option value="sucursal 3">Sucursal3</option>
-  </select>
-  </div>
-  <div class="mb-3">
-  <label for="sucursaldestino" class="form-label">Sucursal Destino</label>
-  <select class="form-select" aria-label="sucursaldestino" id="sucursaldestino" name="sucursaldestino">
-    <option selected>Seleccione una Sucursal</option>
-    <option value="sucursal1">Sucursal1</option>
-    <option value="sucursal2">Sucursal2</option>
-    <option value="sucursal3">Sucursal3</option>
+    <option selected disabled>Seleccione una Sucursal</option>
+@foreach($sucursales as $sucursal)
+
+    <option value="{{$sucursal->id}}">{{$sucursal->nombre}}</option>
+ @endforeach
   </select>
   </div>
   <button type="submit" class="btn btn-primary">Enviar</button>
