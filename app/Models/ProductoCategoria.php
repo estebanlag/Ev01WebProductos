7a57448c3d289302e8ce23a2427b1f2702a4c0d7
Wilmer Rodriguez <WilmerR09gmail.com>
2022-08-13 20:36:22 -0400

    modificando modelo porductocategoria

--- app/Models/ProductoCategoria.php
+++ app/Models/ProductoCategoria.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class ProductoCategoria extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'id';
+    protected $table = 'ProductoCategoria';
+
+    public function categorias(){
+        return $this->belongsTo(Categorias::class, 'id_categoria');
+    }
+
+    public function productos(){
+        return $this->belongsTo(Productos::class, 'id_producto');
+    }
 }
