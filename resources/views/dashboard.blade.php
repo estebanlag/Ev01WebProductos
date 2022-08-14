4885ec4a722ecc2d89c9237e9bf78e11869c8958
Braulio Díaz <braulio.diaz.gajardo@ciisa.cl>
2022-08-13 23:42:37 -0400

    Se agregó la ruta prodsucursal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,7 @@
             <a class="nav-link" href="">Dashboard</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-primary" href="#">Producto Sucursal</a>
+            <a class="nav-link text-primary" href="prodsucursal/create">Producto Sucursal</a>
           </li>
         </ul>
       </div>
