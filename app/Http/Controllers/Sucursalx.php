3bdcb045342e8a992568b2f5c88b07d18478d7e5
seba5197 <sebastian5197@gmail.com>
2022-08-14 01:26:56 -0400

    xxx

--- app/Http/Controllers/Sucursalx.php
+++ app/Http/Controllers/Sucursalx.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-use DB;
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Http\Request;
 use App\Models\Sucursal;
+use Illuminate\Support\Facades\DB as FacadesDB;
 
 class Sucursalx extends Controller
 {
