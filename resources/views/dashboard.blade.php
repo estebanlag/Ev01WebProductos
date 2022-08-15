0ab7f16c2ee2c43e202fdf8cffcfb84662e75670
Wilmer Rodriguez <WilmerR09gmail.com>
2022-08-14 22:35:09 -0400

    actualizando navbar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,56 +8,30 @@
 @section('header')
 
 <nav class="navbar navbar-expand-lg bg-light">
-
-
-    <div class="container-fluid">
-
-      <div class="collapse navbar-collapse" id="navbarNav">
-
-        <ul class="navbar-nav">
-
-          <li class="nav-item">
-
-            <a class="nav-link" href="">Dashboard</a>
-
-          </li>
-
-          <li class="nav-item">
-
-            <a class="nav-link text-primary" href="prodsucursal">Producto Sucursal</a>
-
-          </li>
-
-          <li class="nav-item">
-
-            <a class="nav-link text-primary" href="/mostrarproducto">Ver tabla producto</a>
-
-          </li>
-
-          
-
-        </ul>
-
-      </div>
-
-      <div>
-
-        <form action='/logout' method="POST">
-
-          <a href="#" onclick="this.closest('form').submit()" class="text-decoration-none">
-
-             Logout
-
-          </a>
-
-        </form>
-
-      </div>
-
-    </div>
-
-
+  <div class="container-fluid">
+    <a class="navbar-brand" href="dashboard">Dashboard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="prodsucursal">Producto Sucursal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="mostrarproducto">Ver tabla producto</a>
+        </li>
+        <li class="nav-item">
+          <form action='/logout' method="POST">
+            <a href="" onclick="this.closest('form').submit()" class="nav-link active">Logout</a>
+          </form>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
+<br>
+
 
 @stop
 
